1acb1bd5a077615ef66a2fb15094a22e6476f23e
FEK10 <krogh.frederik@gmail.com>
2016-06-14 11:32:26 +0200

    Create Openorienteering_da.ts
    Danish translation

---
+++ translations/Openorienteering_da.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>AutosaveDialog</name>
+    <message numerus="yes">
+        <location filename="../src/gui/autosave_dialog.cpp" line="45"/>
+        <location filename="../src/gui/autosave_dialog.cpp" line="51"/>
+        <source>%n bytes</source>
+        <translation>Omkring 1%</translation>
+    </message>
+</context>
+<context>
+    <name>Importer</name>
+    <message numerus="yes">
+        <location filename="../src/file_import_export.cpp" line="93"/>
+        <source>Dropped %n irregular object(s).</source>
+        <translation>Smider Irrelevante objekter.</translation>
+    </message>
+</context>
+<context>
+    <name>OCAD8FileImport</name>
+    <message numerus="yes">
+        <location filename="../src/file_format_ocad8.cpp" line="145"/>
+        <source>%n color separation(s) were skipped, reason: Import disabled.</source>
+        <translation>farveseperering er blevet sprunget over, årsag: Importering slået fra.</translation>
+    </message>
+</context>
+<context>
+    <name>TemplateTrack</name>
+    <message numerus="yes">
+        <location filename="../src/template_track.cpp" line="479"/>
+        <source>%n path object(s) could not be imported (reason: missing coordinates).</source>
+        <translation>Sti objekter blev ikke importeret (årsag: manglende koordinater).</translation>
+    </message>
+</context>
+</TS>
